6d72e6816e2b937c70d607a6fe90b4eccc06d2ff
Érika Oliveira <erika_s_oliveira@estudante.sc.senai.br>
2019-09-30 21:52:45 -0300

    update index

--- index.php
+++ index.php
@@ -16,10 +16,12 @@
 
     <title>Sistema de Login</title>
     <style>
-        #alerta, #caixaSenha, #caixaRegistro{
-            display:none;
+        #alerta,
+        #caixaSenha,
+        #caixaRegistro {
+            display: none;
 
-        }    
+        }
     </style>
 </head>
 
@@ -199,6 +201,32 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <script>
+        //Código jQuery para mostrar e ocultar os formulários
+        $(function() {
+            $("#btnEsqueci").click(function() {
+                $("#caixaLogin").hide(); //Ocultar
+                $("#caixaSenha").show(); //Mostrar
+            });
+
+            $("#btnJaRegistrado").click(function() {
+                $("#caixaSenha").hide(); //Ocultar gerar nova senha
+                $("#caixaLogin").show(); //Mostrar caixa login
+            });
+
+
+            $("btnRegistrarNovo").click(function() {
+                $("#caixaLogin").hide(); //Ocultar
+                $("#caixaRegistro").show();
+            });
+
+            $("btnJaRegistrado2").click(function() {
+                $("#caixaLogin").show(); //Mostrar
+                $("#caixaRegistro").hide();//Ocultar
+            });
+        });
+    </script>
 </body>
 
 </html>
